2b5359e6ea26fec2db6750af8aee55a7e0c484c6
John <fluttershy1337@hotmail.com>
2017-03-27 23:33:46 +0200

    so far so good

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import numpy as np
 import networkx as nx
+import matplotlib.pyplot as plt
+from mpl_toolkits.basemap import Basemap as Basemap
+
 
 routes_headers = ['AIRLINE_CODE', 'SOURCE_CODE', 'DESTINATION_CODE', 'DISTANCE', 'TIME']
 routes = pd.read_table('routes.txt', delimiter=';',decimal='.', dtype={'DISTANCE' : float}, header=None, names=routes_headers)
@@ -14,10 +17,10 @@ G.add_nodes_from(airports)
 
 for route in routes.iterrows():
     route = route[1]
+
     attributes = {'AIRLINE_CODE' : route['AIRLINE_CODE'], 'DISTANCE' : route['DISTANCE'], 'TIME' : route['TIME']}
 
     G.add_edge(route['SOURCE_CODE'], route['DESTINATION_CODE'], weight=attributes)
-
 
 for u,v,a in G.edges(data=True):
     weight = a['weight']
@@ -25,4 +28,33 @@ for u,v,a in G.edges(data=True):
     distance = weight['DISTANCE']
     time = weight['TIME']
     s = 'From:', airport_dict.get(u), 'To:', airport_dict.get(v), 'Airline:', airline, 'Distance:', distance, 'Time:', time
-    print(s)
+    # print(s)
+
+def draw_graph():
+    m = Basemap(
+        projection='merc',
+        resolution='i',
+        suppress_ticks=True)
+
+    pos={}
+    for u,v,a in G.edges(data=True):
+        if (u == 'NAME' or v == 'NAME'):
+            print(u, v)
+        lats = [float(airports[airports['CODE'] == u]['LATITUDE']), float(airports[airports['CODE'] == v]['LATITUDE'])]
+        lons = [float(airports[airports['CODE'] == u]['LONGITUDE']), float(airports[airports['CODE'] == v]['LONGITUDE'])]
+
+        mx, my = m(lons, lats)
+        pos[u] = (mx[0], my[0])
+        pos[v] = (mx[1], my[1])
+
+    # draw
+    nx.draw_networkx(G,pos,node_size=100,node_color='blue')
+
+    # Now draw the map
+    m.drawcountries()
+    m.drawstates()
+    m.bluemarble()
+    plt.title('How to get from point a to point b')
+    plt.show()
+
+draw_graph()
